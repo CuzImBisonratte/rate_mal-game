35f4725302ecc94c444b5d252510ca17c58e4131
CuzImBisonratte <CuzImBisonratte@gmail.com>
2022-07-25 17:40:45 +0200

    added user joining mechanism

---
+++ user/joinsession.php
@@ -0,0 +1,41 @@
+<?php 
+
+    // Get the userinput
+    $username = $_POST["username"];
+    $session_id = $_POST["session_id"];
+
+    // Get db keys
+    require_once("../config.php");
+
+    // Conect to database
+    $con = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+    if (mysqli_connect_errno()) {
+        exit("Fehler");
+    }
+
+    // Check if session with id exists
+    if ($stmt = $con->prepare("SELECT * FROM ".$DB_SESSION_TABLE." WHERE session_id = ?")) {
+        $stmt->bind_param('s', $session_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows == 0) {
+            // Unknown session ID
+        } else {
+            // Close the check-statement
+            $stmt->close();
+
+            // Insert session to DB
+            if ($stmt = $con->prepare('INSERT INTO '.$DB_USERS_TABLE.' (username, session_id) VALUES (?, ?)')) {
+                $stmt->bind_param('ss', $username, $session_id);
+                $stmt->execute();
+
+                // close the statement
+                $stmt->close();
+
+            } else {
+                // Log the error
+                exit("Error-".mysqli_error($con));
+            }
+        }
+    }
+?>
